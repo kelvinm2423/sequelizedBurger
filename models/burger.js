570d7279c9213015d1bc5a6f8666e387a0e0d939
Kelvin Medina <kelvinm2423@gmail.com>
2017-05-22 22:03:56 -0400

    HW Submission

---
+++ models/burger.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, DataTypes) {
+  var burgers = sequelize.define('burgers', {
+    burger_name: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN
+  }, {
+    classMethods: {
+      associate: function(models) {
+      }
+    }
+  });
+  return burgers;
+};
